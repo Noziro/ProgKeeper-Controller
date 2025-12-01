25a28b8fb7f88fb29f664f67f24a2ebdcd9ed6a9
Noziro <NoziroRed@gmail.com>
2025-11-30 18:18:12 -0800

    mock-up more endpoints

--- progkeeper/api.py
+++ progkeeper/api.py
@@ -1,7 +1,133 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+	title="ProgKeeper API",
+	version="0.1.0",
+	description="API for controlling all ProgKeeper functions, including frontend data and backend tasks.",
+)
+
+class APIResult:
+	def __init__(self, ok:bool, message:str):
+		if not isinstance(ok, bool):
+			raise ValueError("APIResult.ok must be a boolean value")
+		if not isinstance(message, str):
+			raise ValueError("APIResult.message must be a string")
+		self.ok = ok
+		self.message = message
+
+class UserLogin(BaseModel):
+	username: str
+	password: str
+
+class UserRegister(UserLogin):
+	nickname: str | None = None
+
+
+
+# Meta information
 
 @app.get("/")
-def read_root():
-	return {"message": "API is running."}
+def status():
+	return APIResult(True, "API is running. Visit /docs for API documentation.")
+
+
+
+# Session management
+
+@app.get("/session/end")
+def logout():
+	""" Logout the current user session. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.get("/session/end/all")
+def logout_all():
+	""" Logout from all sessions belonging to this user. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.post("/session/create")
+def login(user: UserLogin):
+	""" Begin a new user session (login). """
+	return APIResult(False, "Not implemented yet.")
+
+
+
+# User management
+
+@app.get("/user/get/{user_id}")
+def get_user(user_id: int):
+	""" Get info about a user. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.post("/user/update/{user_id}")
+def update_user(user_id: int):
+	""" Update user info. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.delete("/user/delete/{user_id}")
+def delete_user(user_id: int):
+	""" Delete user. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.post("/user/create")
+def register(user: UserRegister):
+	""" Register a new user. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.post("/user/import")
+def import_data():
+	""" Import a variety of data to your user account. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.get("/user/export")
+def export_data():
+	""" Import a variety of data to your user account. """
+	return APIResult(False, "Not implemented yet.")
+
+
+
+# Media management
+
+@app.post("/media/create")
+def create_media():
+	""" Create a media item. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.get("/media/get/{media_id}")
+def get_media(media_id: int):
+	""" Get info about a media item. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.post("/media/update/{media_id}")
+def update_media(media_id: int):
+	""" Update info about a media item. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.delete("/media/delete/{media_id}")
+def delete_media(media_id: int):
+	""" Delete a media item. """
+	return APIResult(False, "Not implemented yet.")
+
+
+
+# Collection management
+
+@app.post("/collection/create")
+def create_collection():
+	""" Create a collection. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.get("/collection/get/{collection_id}")
+def get_media(collection_id: int):
+	""" Get info about a collection. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.post("/collection/update/{collection_id}")
+def update_media(collection_id: int):
+	""" Update info about a collection. """
+	return APIResult(False, "Not implemented yet.")
+
+@app.delete("/collection/delete/{collection_id}")
+def delete_media(collection_id: int):
+	""" Delete a collection. """
+	return APIResult(False, "Not implemented yet.")
