b9dde1b995c2ef975e484e8c4116260b2c366f5f
Noziro <NoziroRed@gmail.com>
2025-12-06 15:37:43 -0800

    fix user deletion not actually deleting this mistake somehow made it's way into main from a deleted branch

--- progkeeper/database/user.py
+++ progkeeper/database/user.py
@@ -1,4 +1,5 @@
 from progkeeper.database.common import DatabaseSession
+from typing import Any
 
 def get_user_info(user_id: int) -> dict:
 	""" Get information about a user by their ID. Returns an empty dict if not found. """
@@ -12,12 +13,12 @@ def get_user_info(user_id: int) -> dict:
 # imports specifically for obliterate_user
 from progkeeper.database.auth import delete_all_sessions_for_user
 
-def obliterate_user(user_id: int) -> {}:
+def obliterate_user(user_id: int) -> dict[str, Any]:
 	""" Deletes a user and all of their content. "To shreds, you say?"
 	
 	Returns a dictionary containing data of all performed changes, with the primary attribute {"deleted_user_id": user_id} """
 	
-	data = {}
+	data:dict[str, Any] = {}
 
 	# remove all references to user ID in other tables to allow user deletion
 	# TODO: as other functions are added such as media and collections, you will have to delete these too.
@@ -25,7 +26,7 @@ def obliterate_user(user_id: int) -> {}:
 
 	with DatabaseSession() as db:
 		db.cursor.execute(
-			"UPDATE users SET deleted=true WHERE id = ? AND deleted=false",
+			"DELETE FROM users WHERE id = ?",
 			[user_id]
 		)
 		db.connection.commit()
